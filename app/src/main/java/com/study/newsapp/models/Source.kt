dd45594164d78036c5c424e2f8ad2fcdcd47be27
Vlad <vladvolkov99@gmail.com>
2024-04-10 02:12:50 +0300

    Lesson-2 "API, create models, setup Retrofit"

---
+++ app/src/main/java/com/study/newsapp/models/Source.kt
@@ -0,0 +1,6 @@
+package com.study.newsapp.models
+
+data class Source(
+    val id: String,
+    val name: String
+)
---
+++ app/src/main/java/com/study/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.study.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "99c0c8a1291e48c39d11b576b7a209af"
+    }
+}
